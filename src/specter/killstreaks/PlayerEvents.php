4ee3b833fb35290644d5a7a81ee16b8273e55270
Fris <ilyesou.cherfaoui.18@gmail.com>
2017-12-07 19:55:48 +0200

    Some fixes and updates

--- src/specter/killstreaks/PlayerEvents.php
+++ src/specter/killstreaks/PlayerEvents.php
@@ -4,54 +4,71 @@ namespace specter\killstreaks;
 
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\Player;
+use pocketmine\utils\Config;
 
 class PlayerEvents implements Listener {
-	
-	public $pl;
-	
-	public function __construct(Main $pg) {
-		$this->pl = $pg;
-	}
-	
-	public function onJoin(PlayerJoinEvent $ev){
-	if(!$this->pl->streaks->get(strtolower($ev->getPlayer()->getName()))){
-	  $this->pl->players[strtolower($ev->getPlayer()->getName())] = 0;
-	}else{
-	$this->pl->players[strtolower($ev->getPlayer()->getName())] = $this->pl->streaks->get(strtolower($ev->getPlayer()->getName()));
-	}
-	}
-	
-	public function onDeath(PlayerDeathEvent $event){
-	$p = $event->getEntity();
-	if($p->getLastDamageCause() instanceof EntityDamageByEntityEvent){
-		$killer = $p->getLastDamageCause()->getDamager();
-		if($killer instanceof Player){
-			if(strtolower($killer->getName()) != strtolower($p->getName())){
-			    if($pstreak = $this->pl->getStreak($p) != 0){
-			      	$this->pl->resetStreak($p);
-				$message = $this->pl->config->get("streak-lose-message");
-				$msg = str_replace("{streak}", "{$pstreak}", $message);
-			      	$p->sendMessage($msg);
-			    }
-				$this->pl->addStreak($killer);
-			    if($kstreak = $this->pl->getStreak($killer) != 0){
-				$message = $this->pl->config->get("on-streak-message");
-				$msg = str_replace("{streak}", "{$kstreak}", $message);
-				$killer->sendMessage($msg);
-				if($comm = $this->pl->config->get($kstreak)){
-				$command = str_replace("{player}", "{$killer->getName()}", $comm);
-				$command = str_replace("{streak}", "{$kstreak}", $command);
-				$this->pl->getServer()->dispatchCommand(new ConsoleCommandSender(), "$command");
-			      	}
-			    }
-			}
-		}
-	}
-	}
+
+    public $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->setPlugin($plugin);
+    }
+
+    /**
+     * @param PlayerJoinEvent $ev
+     */
+    public function onJoin(PlayerJoinEvent $ev){
+        if ($this->getPlugin()->streaks instanceof Config) {
+            if ($this->getPlugin()->streaks->exists($ev->getPlayer()->getLowerCaseName(), true)) {
+                $this->getPlugin()->players[$ev->getPlayer()->getLowerCaseName()] = $this->getPlugin()->streaks->get(strtolower($ev->getPlayer()->getName()));
+            } else {
+                $this->getPlugin()->players[$ev->getPlayer()->getLowerCaseName()] = 0;
+            }
+        }
+    }
+
+    /**
+     * @param PlayerDeathEvent $event
+     */
+    public function onDeath(PlayerDeathEvent $event){
+        $p = $event->getPlayer();
+        if($p->getLastDamageCause() instanceof EntityDamageByEntityEvent){
+            $killer = $p->getLastDamageCause()->getDamager();
+            if($killer instanceof Player){
+                if(strtolower($killer->getName()) != strtolower($p->getName())){
+                    if($pstreak = $this->getPlugin()->getStreak($p) != 0){
+                        $this->getPlugin()->resetStreak($p);
+                        $p->sendMessage(str_replace("{streak}", "{$pstreak}", $this->getPlugin()->config->get("streak-lose-message")));
+                    }
+                    $this->getPlugin()->addStreak($killer);
+                    if($kstreak = $this->getPlugin()->getStreak($killer) != 0){
+                        $killer->sendMessage(str_replace("{streak}", "{$kstreak}", $this->getPlugin()->config->get("on-streak-message")));
+                        if($command = $this->getPlugin()->config->get($kstreak)){
+                            $this->getPlugin()->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{streak}", "{$kstreak}", str_replace("{player}", "{$killer->getName()}", $command)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return Main
+     */
+    public function getPlugin() : Main
+    {
+        return $this->plugin;
+    }
+
+    /**
+     * @param Main $plugin
+     */
+    public function setPlugin(Main $plugin)
+    {
+        $this->plugin = $plugin;
+    }
 }
