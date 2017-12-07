67a8f7d3403ebdf714d2f9487284ef9b8cb03304
Infernus <Infernus101@users.noreply.github.com>
2017-12-07 21:56:31 +0530

    Create PlayerEvents.php

---
+++ src/Infernus101/PlayerEvents.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Infernus101;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\Player;
+
+class PlayerEvents implements Listener {
+	
+	public $pl;
+	
+	public function __construct(Main $pg) {
+		$this->pl = $pg;
+	}
+	
+	public function onJoin(PlayerJoinEvent $ev){
+			if(!$this->pl->streaks->get(strtolower($ev->getPlayer()->getName()))){
+			  $this->pl->players[strtolower($ev->getPlayer()->getName())] = 0;
+			}else{
+        $this->pl->players[strtolower($ev->getPlayer()->getName())] = $this->pl->streaks->get(strtolower($ev->getPlayer()->getName()));
+	}
+	
+	public function onDeath(PlayerDeathEvent $event){
+			$p = $event->getEntity();
+			if($p->getLastDamageCause() instanceof EntityDamageByEntityEvent){
+				$killer = $p->getLastDamageCause()->getDamager();
+				if($killer instanceof Player){
+					if(strtolower($killer->getName()) != strtolower($p->getName())){
+            if($pstreak = $this->pl->getStreak($p) != 0){
+              $this->pl->removeStreak($p);
+              $p->sendMessage($this->pl->config->get("streak-lose-message"));
+            }
+					  $this->pl->addStreak($killer);
+            if($kstreak = $this->pl->getStreak($killer) != 0){
+              if($comm = $this->pl->config->get($kstreak)){
+                $command = str_replace("{player}", "{$killer->getName()}", $comm);
+                $this->pl->getServer()->dispatchCommand(new ConsoleCommandSender(), "$command");
+              }
+            }
+					}
+				}
+			}
+	}
+}
